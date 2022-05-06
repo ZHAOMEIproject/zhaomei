408f11bf59ddaad02598b12c423c85d466c59b4b
zwjkflc <503755414@qq.com>
2022-05-06 02:57:05 +0000

    new

--- owl-nft/hardhat.config.js
+++ owl-nft/hardhat.config.js
@@ -1,4 +1,11 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require('hardhat-abi-exporter');
+// require("./task/counter.js");
+const fs = require('fs');
+const mnemonic =fs.readFileSync("/home/ubuntu/zwj-learn/.secret").toString().trim();
+const infrakey='';
+const scankey='';
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -16,6 +23,44 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.8.4",
+ module.exports = {
+  solidity:{
+    compilers:[
+      {
+        version: "0.8.7",
+        setting:{
+          optimizer:{
+            enable:true,
+            runs:200
+          }
+        }
+      }
+    ]
+  },
+  networks:{
+    dev:{
+      url:"http://127.0.0.1:8545",
+      chainId:31337,
+    },
+    oktest:{
+      url:"https://exchaintestrpc.okex.org",
+      chainId:65,
+      accounts:{
+        mnemonic:mnemonic,
+      },
+    },
+    bnbtest:{
+      url:"https://data-seed-prebsc-1-s1.binance.org:8545/",
+      chainId:97,
+      accounts:{
+        mnemonic:mnemonic,
+      },
+    }
+  },
+  etherscan:{
+    apiKey:{
+      bnbtest: 'AXUTCF27F9PY5VJHE25J6Q4HB6DIG9DQWH',
+      ropsten:"7B8S6DPCHKEE5FGWX9JPR7Z9KNB44Q9CIS"
+    }
+  }
 };

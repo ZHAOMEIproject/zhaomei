177a8703eed97764c7a54ccae56e5252238c0840
ZWJKFLC <503755414@qq.com>
2022-06-27 11:00:59 +0800

    update

--- VII_Wallet/node/api/restApi.js
+++ VII_Wallet/node/api/restApi.js
@@ -2,5 +2,9 @@
 exports.restApiConfig = function restApiConfig(app){
     app.use("/v1/withdraw", require("./withdraw/withdraw"));
 
+    app.use("/v1/contractapi", require("./contractapi"));
+
+
+
     console.log(`RestApi start loading`);
 }

4574ebf5717be8ecde52ce3a5b5facc24c3a1e90
ZWJKFLC <503755414@qq.com>
2022-07-07 15:58:00 +0800

    update

--- Vii_Owl/Hardhat_Contract/contracts/owl_simple.sol
+++ Vii_Owl/Hardhat_Contract/contracts/owl_simple.sol
@@ -8,8 +8,21 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract VII_OWL is ERC721, ERC721Burnable, Ownable{
     using Counters for Counters.Counter;
+    using Strings for uint256;
     Counters.Counter private _tokenIdCounter;
-    constructor() ERC721("VII_OWL", "VOL") {}
+    constructor() ERC721("VII_OWL", "VOL") {
+        safeMint(msg.sender);
+        // set_baseinfo(
+        //     "ipfs://bafybeiasstkn5xlajj43nxzokxtv4sbjfp3j6cwvgbc6ui5immwbbfd2fq/",
+        //     false,
+        //     0
+        // );
+        set_baseinfo(
+            "ipfs://bafybeiasstkn5xlajj43nxzokxtv4sbjfp3j6cwvgbc6ui5immwbbfd2fq/",
+            true,
+            0
+        );
+    }
     string baseURL;
     bool egg_open;
     uint256 public sell_price;
@@ -19,10 +32,15 @@ contract VII_OWL is ERC721, ERC721Burnable, Ownable{
         sell_price=_sell_price;
     }
     function _baseURI() internal view override returns (string memory) {
-        if(!egg_open){
-            return "ipfs://bafybeiavjuajpj3hhtnca4v3fsqq2jrxwtgg2lvw7yczrdx7mltd2n74ua/nftowlegg.json";
-        }
         return baseURL;
+    }
+    function tokenURI(uint256 tokenId)public view override returns (string memory){
+         if(egg_open){
+            require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+            string memory baseURI = _baseURI();
+            return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString(),".json")) : "";
+         }
+         return "ipfs://bafybeiavjuajpj3hhtnca4v3fsqq2jrxwtgg2lvw7yczrdx7mltd2n74ua/nftowlegg.json";
     }
     function safeMint(address to) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();

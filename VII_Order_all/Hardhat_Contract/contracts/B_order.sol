61d1f0d89067bd9f415af29551f0bdfc034867c6
ZWJKFLC <503755414@qq.com>
2022-07-09 13:33:06 +0800

    update

--- VII_Order_all/Hardhat_Contract/contracts/B_order.sol
+++ VII_Order_all/Hardhat_Contract/contracts/B_order.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >0.8.4;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+pragma solidity >=0.8.15;
 
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
+
+// npx hardhat run scripts/1_develop_main.js --network bnbtest
+// npx hardhat verify 0xd6F596C7E3eb6EadeaE62d6952B83b994665074b --network bnbtest
 
 // contract B_order{
 contract B_order is EIP712{
@@ -12,11 +13,11 @@ contract B_order is EIP712{
         owner=msg.sender;
     }
 
-    address public owner;
+    address private owner;
     
     address constant public weth=0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
-    address constant public usdc=0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d;
-    address constant public pair=0xd99c7F6C65857AC913a8f880A4cb84032AB2FC5b;
+    address constant private usdc=0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d;
+    address constant private pair=0xd99c7F6C65857AC913a8f880A4cb84032AB2FC5b;
 
     bytes32 private constant _PERMIT_TYPEHASH =
         keccak256("order(uint256 order,uint256 amount,uint256 deadline)");
@@ -25,26 +26,29 @@ contract B_order is EIP712{
     mapping(uint256=>uint256) public order_state;
 
     function eorder(uint256 order,uint256 amount ,uint256 deadline ,uint8 v,bytes32 r,bytes32 s)payable public{
-        require(msg.sender.code.length == 0,"order: can't use contract");
+        require(tx.origin==msg.sender,"order: can't use contract");
         require(deadline>block.timestamp,"order: time error");
         check(order,amount,deadline,v,r,s);
-        uint256 eamount = amount*10**ERC20(usdc).decimals()/ethprice();
-        require(msg.value>=eamount,"order: error eth amount");
-        payable(msg.sender).transfer(msg.value-eamount);
-        payable(owner).transfer(address(this).balance);
-
-        require(order_state[order]==0,"order: order completed");
-        order_state[order]=amount;
-        emit Order(order,amount);
+        uint256 eamount;
+        unchecked{
+            eamount = amount*uethprice()/10**IERC20(usdc).decimals();
+            require(msg.value>=eamount,"order: error eth amount");
+            payable(msg.sender).transfer(msg.value-eamount);
+            payable(owner).transfer(address(this).balance);
+            require(order_state[order]==0,"order: order completed");
+            order_state[order] = amount*(10**(18-IERC20(usdc).decimals()));
+        }
+        emit Order(order,order_state[order]);
     }
 
     function uorder(uint256 order,uint256 amount ,uint256 deadline ,uint8 v,bytes32 r,bytes32 s)public{
         require(deadline>block.timestamp,"order: time error");
         check(order,amount,deadline,v,r,s);
-        ERC20(usdc).transferFrom(msg.sender,owner,amount);
-
+        IERC20(usdc).transferFrom(msg.sender,owner,amount);
         require(order_state[order]==0,"order: order completed");
-        order_state[order]=amount;
+        unchecked{
+            order_state[order] = amount*(10**(18-IERC20(usdc).decimals()));
+        }
         emit Order(order,amount);
     }
 
@@ -54,9 +58,27 @@ contract B_order is EIP712{
         address signer = ECDSA.recover(hash, v, r, s);
         require(signer == owner, "order: signer invalid signature");
     }
-    function ethprice()view public returns(uint256 price){
-        uint256 e_balance = ERC20(weth).balanceOf(pair);
-        uint256 u_balance = ERC20(usdc).balanceOf(pair);
-        price = u_balance*10**ERC20(usdc).decimals()/e_balance;
+    function uethprice()view public returns(uint256 price){
+        unchecked{
+            price = IERC20(weth).balanceOf(pair)*
+            10**IERC20(usdc).decimals()/
+            IERC20(usdc).balanceOf(pair);
+        }
     }
+}
+
+interface IERC20 {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    function decimals() external view returns (uint256);
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address to, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
 }
--- VII_Order_all/Hardhat_Contract/contracts/E_order.sol
+++ VII_Order_all/Hardhat_Contract/contracts/E_order.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >0.8.4;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+pragma solidity >=0.8.15;
 
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
+
+// npx hardhat run scripts/1_develop_main.js --network bnbtest
+// npx hardhat verify 0xd6F596C7E3eb6EadeaE62d6952B83b994665074b --network bnbtest
 
 // contract B_order{
 contract E_order is EIP712{
@@ -12,11 +13,11 @@ contract E_order is EIP712{
         owner=msg.sender;
     }
 
-    address public owner;
+    address private owner;
 
     address constant public weth=0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    address constant public usdc=0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-    address constant public pair=0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc;
+    address constant private usdc=0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address constant private pair=0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc;
 
     bytes32 private constant _PERMIT_TYPEHASH =
         keccak256("order(uint256 order,uint256 amount,uint256 deadline)");
@@ -25,26 +26,29 @@ contract E_order is EIP712{
     mapping(uint256=>uint256) public order_state;
 
     function eorder(uint256 order,uint256 amount ,uint256 deadline ,uint8 v,bytes32 r,bytes32 s)payable public{
-        require(msg.sender.code.length == 0,"order: can't use contract");
+        require(tx.origin==msg.sender,"order: can't use contract");
         require(deadline>block.timestamp,"order: time error");
         check(order,amount,deadline,v,r,s);
-        uint256 eamount = amount*10**ERC20(usdc).decimals()/ethprice();
-        require(msg.value>=eamount,"order: error eth amount");
-        payable(msg.sender).transfer(msg.value-eamount);
-        payable(owner).transfer(address(this).balance);
-
-        require(order_state[order]==0,"order: order completed");
-        order_state[order]=amount;
-        emit Order(order,amount);
+        uint256 eamount;
+        unchecked{
+            eamount = amount*uethprice()/10**IERC20(usdc).decimals();
+            require(msg.value>=eamount,"order: error eth amount");
+            payable(msg.sender).transfer(msg.value-eamount);
+            payable(owner).transfer(address(this).balance);
+            require(order_state[order]==0,"order: order completed");
+            order_state[order] = amount*(10**(18-IERC20(usdc).decimals()));
+        }
+        emit Order(order,order_state[order]);
     }
 
     function uorder(uint256 order,uint256 amount ,uint256 deadline ,uint8 v,bytes32 r,bytes32 s)public{
         require(deadline>block.timestamp,"order: time error");
         check(order,amount,deadline,v,r,s);
-        ERC20(usdc).transferFrom(msg.sender,owner,amount);
-
+        IERC20(usdc).transferFrom(msg.sender,owner,amount);
         require(order_state[order]==0,"order: order completed");
-        order_state[order]=amount;
+        unchecked{
+            order_state[order] = amount*(10**(18-IERC20(usdc).decimals()));
+        }
         emit Order(order,amount);
     }
 
@@ -54,9 +58,27 @@ contract E_order is EIP712{
         address signer = ECDSA.recover(hash, v, r, s);
         require(signer == owner, "order: signer invalid signature");
     }
-    function ethprice()view public returns(uint256 price){
-        uint256 e_balance = ERC20(weth).balanceOf(pair);
-        uint256 u_balance = ERC20(usdc).balanceOf(pair);
-        price = u_balance*10**ERC20(usdc).decimals()/e_balance;
+    function uethprice()view public returns(uint256 price){
+        unchecked{
+            price = IERC20(weth).balanceOf(pair)*
+            10**IERC20(usdc).decimals()/
+            IERC20(usdc).balanceOf(pair);
+        }
     }
+}
+
+interface IERC20 {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    function decimals() external view returns (uint256);
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address to, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
 }

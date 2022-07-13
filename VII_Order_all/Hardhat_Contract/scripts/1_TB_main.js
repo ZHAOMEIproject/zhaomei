b13dc87be3b1d09b96c338dee4b26c567f93f512
ZWJKFLC <503755414@qq.com>
2022-07-13 17:26:31 +0800

    update

--- VII_Order_all/Hardhat_Contract/scripts/1_TB_main.js
+++ VII_Order_all/Hardhat_Contract/scripts/1_TB_main.js
@@ -2,9 +2,10 @@ const hre = require("hardhat");
 const { writer_info_all } = require('./tool/hh_log.js');
 
 async function main(){
-  let [owner, addr1, addr2] = await ethers.getSigners();
+  // let [owner, addr1, addr2] = await ethers.getSigners();
   const TB_order = await hre.ethers.getContractFactory("TB_order");
-  const tb_order = await TB_order.connect(addr2).deploy({gasPrice: 10000000000});
+  // const tb_order = await TB_order.connect(addr2).deploy({gasPrice: 10000000000});
+  const tb_order = await TB_order.deploy({gasPrice: 10000000000});
   await tb_order.deployed();
   console.log("TB_order deployed to:", tb_order.address);
   let Artifact = await artifacts.readArtifact("TB_order");
--- VII_Order_all/Hardhat_Contract/scripts/1_TE_main.js
+++ VII_Order_all/Hardhat_Contract/scripts/1_TE_main.js
@@ -2,9 +2,10 @@ const hre = require("hardhat");
 const { writer_info_all } = require('./tool/hh_log.js');
 
 async function main(){
-  let [owner, addr1, addr2] = await ethers.getSigners();
+  // let [owner, addr1, addr2] = await ethers.getSigners();
   const TE_order = await hre.ethers.getContractFactory("TE_order");
-  const te_order = await TE_order.connect(addr2).deploy();
+  // const te_order = await TE_order.connect(addr2).deploy();
+  const te_order = await TE_order.deploy();
   await te_order.deployed();
   console.log("TE_order deployed to:", te_order.address);
   let Artifact = await artifacts.readArtifact("TE_order");

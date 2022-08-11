938471555344e6f0c4aee076fc7d7857b853fb31
ZWJKFLC <503755414@qq.com>
2022-08-11 14:05:47 +0800

    update

--- Free_mint/node/task/scanblock/fun/scanblock.js
+++ Free_mint/node/task/scanblock/fun/scanblock.js
@@ -1,11 +1,24 @@
 var Web3 = require('web3');
-var web3 = new Web3("wss://eth-mainnet.g.alchemy.com/v2/k4k7w92FZHQtnAOKcr_q5LW3SxHhrqD2");
+// var web3 = new Web3("wss://eth-mainnet.g.alchemy.com/v2/k4k7w92FZHQtnAOKcr_q5LW3SxHhrqD2");
+var web3 = new Web3("https://api.mycryptoapi.com/eth");
 
 exports.scanblock = async function scanblock(){
     // let nowblocknumber = await web3.eth.getBlockNumber();
-    let nowblocknumber = 15313796;
+    let nowblocknumber = 12347219;
     console.log(nowblocknumber);
     let blockinfo = await web3.eth.getBlock(nowblocknumber);
     console.log(blockinfo);
-    console.log(await web3.eth.getTransaction(blockinfo.transactions[0]));
+    let contracttraninfo = await web3.eth.getTransaction(blockinfo.transactions[110])
+    console.log(contracttraninfo);
+    try {
+        let estimate = await web3.eth.estimateGas({
+            to:contracttraninfo.to,
+            data:contracttraninfo.input
+        })
+        console.log(estimate);
+    } catch (error) {
+        console.log(error);
+    }
+    // console.log(estimate);
+    console.log("no out");
 }

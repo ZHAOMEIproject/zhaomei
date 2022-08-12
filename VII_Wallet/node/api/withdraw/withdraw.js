eac20518ad8661534f92fc2961c227e89766caae
ZWJKFLC <503755414@qq.com>
2022-08-12 15:27:04 +0800

    update

--- VII_Wallet/node/api/withdraw/withdraw.js
+++ VII_Wallet/node/api/withdraw/withdraw.js
@@ -8,10 +8,65 @@ const {sendEmail} = require("../../nodetool/email");
 
 
 
+// exports.postwithdraw = router.get("/postwithdrawservicenonce", async (req, res) => {
+//     var params = url.parse(req.url, true).query;
+
+//     let check =["spender","amount","servicenonce","orderid"];
+//     if(!check.every(key=>key in params)){
+//         res.send({
+//             success:false,
+//             error:"error params"
+//         });
+//         return;
+//     }
+//     let sqlparams=[];
+//     for(let i in check){
+//         sqlparams.push(params[check[i]]);
+//     }
+//     // check
+//     let checkservicenonce ="select servicenonce from withdraw ORDER BY servicenonce DESC LIMIT 0,1"
+//     let servicenonce = await conn.select(checkservicenonce,null)
+//     if(servicenonce.length==0){
+//         servicenonce=[{"servicenonce":0}];
+//     }
+//     servicenonce[0].servicenonce++;
+//     // console.log(servicenonce[0].servicenonce+1,params.servicenonce);
+//     if((servicenonce[0].servicenonce!=params.servicenonce)){
+//         res.send({
+//             success:false,
+//             data:{
+//                 error:"error servicenonce",
+//                 servicenonce:servicenonce[0].servicenonce
+//             }
+//         });
+//         sendEmail("Wallet:servicenonce error","error servicenonce");
+//         return;
+//     }
+
+//     let sqlStr = "INSERT INTO withdraw(spender,amount,servicenonce,orderid)VALUES(?,?,?,?)";
+//     try {
+//         await conn.select(sqlStr,sqlparams);
+//     } catch (error) {
+//         res.send({
+//             success:false,
+//             data:{
+//                 error:error
+//             }
+//         });
+//         sendEmail("Wallet:orderid error ","error orderid");
+//         return;
+//     }
+
+//     res.send({
+//         success:true,
+//     });
+//     return;
+// });
+
 exports.postwithdraw = router.get("/postwithdraw", async (req, res) => {
     var params = url.parse(req.url, true).query;
 
-    let check =["spender","amount","servicenonce","orderid"];
+    let check =["spender","amount","orderid"];
     if(!check.every(key=>key in params)){
         res.send({
             success:false,
@@ -23,27 +78,18 @@ exports.postwithdraw = router.get("/postwithdraw", async (req, res) => {
     for(let i in check){
         sqlparams.push(params[check[i]]);
     }
-    // check
-    let checkservicenonce ="select servicenonce from withdraw ORDER BY servicenonce DESC LIMIT 0,1"
-    let servicenonce = await conn.select(checkservicenonce,null)
-    if(servicenonce.length==0){
-        servicenonce=[{"servicenonce":0}];
-    }
-    servicenonce[0].servicenonce++;
-    // console.log(servicenonce[0].servicenonce+1,params.servicenonce);
-    if((servicenonce[0].servicenonce!=params.servicenonce)){
+    let checkorderid ="select orderid from withdraw where orderid=?"
+    let orderidsql = await conn.select(checkorderid,params["orderid"])
+    if(orderidsql.length!=0){
         res.send({
-            success:false,
+            success:true,
             data:{
-                error:"error servicenonce",
-                servicenonce:servicenonce[0].servicenonce
+                result:"Repeated order submission"
             }
         });
-        sendEmail("Wallet:servicenonce error","error servicenonce");
-        return;
     }
 
-    let sqlStr = "INSERT INTO withdraw(spender,amount,servicenonce,orderid)VALUES(?,?,?,?)";
+    let sqlStr = "INSERT INTO withdraw(spender,amount,orderid)VALUES(?,?,?,?)";
     try {
         await conn.select(sqlStr,sqlparams);
     } catch (error) {
@@ -78,6 +124,18 @@ exports.postwithdraw = router.get("/postwithdrawsign", async (req, res) => {
     for(let i in check){
         sqlparams.push(params[check[i]]);
     }
+
+    let checkorderid ="select orderid from withdraw_auditor where orderid=?"
+    let orderidsql = await conn.select(checkorderid,params[orderid])
+    if(orderidsql.length!=0){
+        res.send({
+            success:true,
+            data:{
+                result:"Repeated order submission"
+            }
+        });
+    }
+
     let sqlStr = "INSERT INTO withdraw_auditor("+
     "auditor,spender,amount,auditor_nonce,deadline,sign_r,sign_s,sign_v,orderid)VALUES("+
     "?,?,?,?,?,?,?,?,?)";
@@ -100,21 +158,22 @@ exports.postwithdraw = router.get("/postwithdrawsign", async (req, res) => {
     return;
 });
 
-exports.postwithdraw = router.get("/getwithdrawnonce", async (req, res) => {
-    let sqlStr = "select servicenonce from withdraw ORDER BY servicenonce DESC LIMIT 0,1;";
-    let nonce = await conn.select(sqlStr,null)
-    let s_nonce=0;
-    if(nonce.length!=0){
-        s_nonce=nonce[0].servicenonce
-    }
-    res.send({
-        success:true,
-        data:{
-            nonce:s_nonce,
-        }
-    });
-    return;
-});
+// exports.postwithdraw = router.get("/getwithdrawnonce", async (req, res) => {
+//     let sqlStr = "select servicenonce from withdraw ORDER BY servicenonce DESC LIMIT 0,1;";
+//     let nonce = await conn.select(sqlStr,null)
+//     let s_nonce=0;
+//     if(nonce.length!=0){
+//         s_nonce=nonce[0].servicenonce
+//     }
+//     res.send({
+//         success:true,
+//         data:{
+//             nonce:s_nonce,
+//         }
+//     });
+//     return;
+// });
+
 // const BigNumber = require("bignumber.js");
 exports.postwithdraw = router.post("/checkorderid", async (req, res) => {
     // var params = url.parse(req.body, true).query;

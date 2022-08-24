c436bd0fca7bf1b356e1329c82ee1a8ce39c6580
ZWJKFLC <503755414@qq.com>
2022-08-24 18:15:57 +0800

    update

--- VII_Wallet/Hardhat_Contract/contracts/modular/otherinfo.sol
+++ VII_Wallet/Hardhat_Contract/contracts/modular/otherinfo.sol
@@ -4,8 +4,14 @@ import "../ZM_tool/ZMMainControl.sol";
 
 abstract contract otherinfo is ZMMainControl{
     bytes32 public constant WITHDRAW_ROLE = keccak256("WITHDRAW_ROLE");
-    constructor(){
-        address withdraw=msg.sender;
-        _grantRole(WITHDRAW_ROLE, withdraw);
+    bytes32 public constant AUDITOR_ROLE = keccak256("AUDITOR_ROLE");
+    constructor(address _withdraw,address _admin,address _manage,address _monitor)
+        ZMMainControl(
+            _admin,_manage,_monitor
+        )
+    {
+        // address _withdraw=msg.sender;
+        _grantRole(WITHDRAW_ROLE, _withdraw);
+
     }
 }

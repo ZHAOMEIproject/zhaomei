87b9e181ef0d537396f41c19bc43c85d6cd492d3
ZWJKFLC <503755414@qq.com>
2022-09-16 17:00:28 +0800

    update

--- VII_ERC20/Hardhat_Contract/contracts/Viide.sol
+++ VII_ERC20/Hardhat_Contract/contracts/Viide.sol
@@ -5,8 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
-contract vii_s is ERC20, ERC20Burnable, ERC20Permit {
-    constructor() ERC20("vii", "VID") ERC20Permit("vii") {
+contract Viide is ERC20, ERC20Burnable, ERC20Permit {
+    constructor() ERC20("Viide", "VII") ERC20Permit("VII") {
         _mint(0x758b1B0e6301ec475df75D4C79ac07E184A3C741, 30000000 * 10 ** decimals());
     }
 }

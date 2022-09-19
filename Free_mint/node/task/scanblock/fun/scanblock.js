eb3c1c547631d6c5b0217278e38f86521df0f7d8
ZWJKFLC <503755414@qq.com>
2022-09-19 11:20:16 +0800

    updateupdate

--- Free_mint/node/task/scanblock/fun/scanblock.js
+++ Free_mint/node/task/scanblock/fun/scanblock.js
@@ -3,8 +3,7 @@
 // var web3 = new Web3("wss://eth-mainnet.g.alchemy.com/v2/k4k7w92FZHQtnAOKcr_q5LW3SxHhrqD2");
 const connection = require("../../../nodetool/sqlconnection");
 var iercnft=require("./iercnft.json");
-// const mysql = require("mysql2");
-// const conn = mysql.createConnection(global.mysqlGlobal);
+
 // exports.getblocknumber = async function getblocknumber(){
 //     return await web3.eth.getBlockNumber();
 // }
@@ -12,35 +11,61 @@ exports.scanblock = async function scanblock(){
     var Web3 = require('web3');
     var web3 = new Web3("http://127.0.0.1:8545");
     web3.eth.defaultAccount = "0x8C327f1Aa6327F01A9A74cEc696691cEAAc680e2";
+    const mysql = require("mysql2");
+    const conn = mysql.createConnection(global.mysqlGlobal);
     // await testfun();
     // return;
     let sqlgetblocknumber = "select * from scannumber";
-    let r_sqlblocknumber = await connection.sqlcall(sqlgetblocknumber,null);
+    let r_sqlblocknumber = await connection.sqlcall_uncon(conn,sqlgetblocknumber,null);
     let blocknumber=r_sqlblocknumber[0].blocknumber;
     let nowblocknumber = await web3.eth.getBlockNumber();
+    // console.log("now:",nowblocknumber,"old:",blocknumber);
     if(nowblocknumber<=blocknumber){
         return;
-        return new Promise(function (resolve, reject){
-            return;
-        });
-    }else{
-        nowblocknumber=blocknumber;
-        let sqlupdateblocknumber = "update scannumber set blocknumber=blocknumber+1";
-        await connection.sqlcall(sqlupdateblocknumber,null);
     }
-    // nowblocknumber = 15475522;
-    console.log(nowblocknumber,Date.now());
+    // blocknumber = 15528555;
+    // await eachblock(blocknumber,web3);
+    // return;
+    let bnl =nowblocknumber-blocknumber;
+    for(let i=blocknumber;i<=nowblocknumber;i++){
+        // await wait(1000);
+        await eachblock(i,web3,conn);
+    }
+    // await eachblock(nowblocknumber,web3,conn);
+    // let sqlupdateblocknumber = "update scannumber set blocknumber=? where blocknumber<?";
+    // await connection.sqlcall_uncon(conn,sqlupdateblocknumber,[nowblocknumber,nowblocknumber]);
+    return;
+
+    // nowblocknumber=blocknumber;
+    // let sqlupdateblocknumber = "update scannumber set blocknumber=blocknumber+1";
+    // await connection.sqlcall_uncon(conn,sqlupdateblocknumber,null);
+
+    // // nowblocknumber = 15528539;
+    // console.log(nowblocknumber,Date.now());
     
-    let blockinfo = await web3.eth.getBlock(nowblocknumber);
-    // console.log(blockinfo);
-    // web3.eth.accounts.wallet.add(web3.eth.accounts.create().privateKey);
+    // let blockinfo = await web3.eth.getBlock(nowblocknumber);
+    // // console.log(blockinfo);
+    // // web3.eth.accounts.wallet.add(web3.eth.accounts.create().privateKey);
+    // let l =blockinfo.transactions.length;
+    // // let test=197;
+    // // for(let i=test;i==test;i++){
+    // for(let i in blockinfo.transactions){
+    //     // console.log(i);
+    //     // await wait(10000/l);
+    //     scantransactions(web3,i,blockinfo.transactions[i])
+    // }
+    // return;
+}
+
+async function eachblock(blocknumber,web3,conn){
+    console.log(blocknumber,Date.now());
+    let sqlupdateblocknumber = "update scannumber set blocknumber=? where blocknumber<?";
+    await connection.sqlcall_uncon(conn,sqlupdateblocknumber,[blocknumber,blocknumber]);
+    let blockinfo = await web3.eth.getBlock(blocknumber);
     let l =blockinfo.transactions.length;
-    // let test=197;
-    // for(let i=test;i==test;i++){
     for(let i in blockinfo.transactions){
-        // console.log(i);
-        await wait(10000/l);
-        scantransactions(web3,i,blockinfo.transactions[i])
+        await wait(1000/l);
+        scantransactions(web3,i,blockinfo.transactions[i],conn)
     }
     return;
 }
@@ -59,7 +84,7 @@ async function getconadd(sender,nonce){
     // console.log("contract_address: " + contract_address);
     return "0x"+contract_address;
 }
-async function scantransactions(web3,i,now_transactions){
+async function scantransactions(web3,i,now_transactions,conn){
     // return;
     // console.log(i,now_transactions);
     let contracttraninfo = await web3.eth.getTransaction(now_transactions);
@@ -95,7 +120,7 @@ async function scantransactions(web3,i,now_transactions){
     }
     
     let checkcreat = "select * from nft_address where address=?";
-    let getinfo = await connection.sqlcall(checkcreat,address);
+    let getinfo = await connection.sqlcall_uncon(conn,checkcreat,address);
     // 记录合约信息
     let blocknumber = contracttraninfo.blockNumber;
     if(getinfo.length==0){
@@ -120,11 +145,11 @@ async function scantransactions(web3,i,now_transactions){
 
         let sqlstr="replace into nft_address(address,lasttime,nonce,name,symbol,creater,blocknumber,totalSupply,nfttype)value(?,UNIX_TIMESTAMP(NOW()),?,?,?,?,?,?,?)";
         let sqlp = [address,0,name,symbol,creater,blocknumber,totalsupply,nfttype];
-        await connection.sqlcall(sqlstr,sqlp);
-        getinfo = await connection.sqlcall(checkcreat,address);
+        await connection.sqlcall_uncon(conn,sqlstr,sqlp);
+        getinfo = await connection.sqlcall_uncon(conn,checkcreat,address);
     }else{
         let update="update nft_address set nonce = (select count(1) from nft_trans where timestamp>(UNIX_TIMESTAMP(NOW())-86400)and address=?) where address=?";
-        await connection.sqlcall(update,[address,address]);
+        await connection.sqlcall_uncon(conn,update,[address,address]);
     }
     // let test = blocknumber-1;
     // console.log(test,blocknumber);
@@ -152,26 +177,30 @@ async function scantransactions(web3,i,now_transactions){
             }
             let imageURI;
             if(tokenURI){
-
+                let info
                 if (tokenURI.indexOf("http")!=-1) {
                     try {
-                        let info = await getbyurl(tokenURI);
+                        info = await getbyurl(tokenURI);
                         // console.log(info);
                         imageURI = info.image;
                         if(imageURI.indexOf("ipfs://")!=-1){
                             imageURI="https://ipfs.io/ipfs/"+imageURI.slice(7);
                         }
-                    } catch (error) {console.log("url",tokenURI,error);}
+                    } catch (error) {
+                        // console.log("url",info,tokenURI,error);
+                    }
                     // console.log("1",tokenURI);
                 }else if(tokenURI.indexOf("ipfs://")!=-1){
                     try {
-                        let info = await getbyurl("https://ipfs.io/ipfs/"+tokenURI.slice(7));
+                        info = await getbyurl("https://ipfs.io/ipfs/"+tokenURI.slice(7));
                         // console.log(info);
                         imageURI = info.image;
                         if(imageURI.indexOf("ipfs://")!=-1){
                             imageURI="https://ipfs.io/ipfs/"+imageURI.slice(7);
                         }
-                    } catch (error) {console.log("ipfs",tokenURI,error);}
+                    } catch (error) {
+                        // console.log("ipfs",info,tokenURI,error);
+                    }
                 }
             }
             mint_event.push([blocknumber,address,transactionHash,minter,events[j].returnValues.tokenId,tokenURI,imageURI])
@@ -180,9 +209,9 @@ async function scantransactions(web3,i,now_transactions){
     if (mint_event.length!=0) {
         let insert_event='insert into nft_event(blocknumber,address,transaction,to_add,tokenid,tokenURI,imageURI) values ?';
         // console.log(mint_event);
-        await connection.sqlcall(insert_event,[mint_event]);
+        await connection.sqlcall_uncon(conn,insert_event,[mint_event]);
         let update="update nft_address set totalMinted=totalMinted+? where address=?";
-        await connection.sqlcall(update,[mint_event.length,address]);
+        await connection.sqlcall_uncon(conn,update,[mint_event.length,address]);
     }
     if (mintamount==0) {
         return;
@@ -217,13 +246,13 @@ async function scantransactions(web3,i,now_transactions){
             let sqlin = "insert into nft_trans(blocknumber,address,transaction,to_add,amount,calldata,flag_address,timestamp) VALUES(?,?,?,?,?,?,?,UNIX_TIMESTAMP(NOW()))";
             let inputinfo = [blocknumber,address,transactionHash,minter,mintamount,contracttraninfo.input,flag!=-1]
             // console.log(inputinfo);
-            await connection.sqlcall(sqlin,inputinfo);
+            await connection.sqlcall_uncon(conn,sqlin,inputinfo);
             console.log("success");
             let checkfisttime="select firstMintTime from nft_address where address=?";
-            let check = await connection.sqlcall(checkfisttime,address);
+            let check = await connection.sqlcall_uncon(conn,checkfisttime,address);
             if (check.firstMintTime==null) {
                 let update="update nft_address set firstMintTime=UNIX_TIMESTAMP(NOW()) where address=?";
-                await connection.sqlcall(update,address);
+                await connection.sqlcall_uncon(conn,update,address);
             }
         }
     }
@@ -242,9 +271,18 @@ function getbyurl(url){
             url:url
         },async function (error, response, body) {
             if (!error && response.statusCode == 200) {
-                let json = JSON.parse(body);
+                let info = body.replace(/\n/g,"").replace(/\r/g,"");
+                info = body.replace(/\n/g,"").replace(/\s|\xA0/g,"");
+                let json;
+                try {
+                    json = JSON.parse(info);
+                } catch (error) {
+                    // console.log("getbyurl\n",json,info,error);
+                    // json.image =JSON.parse(body.image)
+                }
                 resolve(json);
             }else{
+                // console.log("error",error);
                 resolve();
             }
         })

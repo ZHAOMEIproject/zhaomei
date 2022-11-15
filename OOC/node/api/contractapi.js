b6778c8d9d80a9876c7ca872725c6e99353abefd
ZWJKFLC <503755414@qq.com>
2022-11-15 12:00:21 +0800

    update

--- OOC/node/api/contractapi.js
+++ OOC/node/api/contractapi.js
@@ -6,7 +6,7 @@ module.exports = router;
 
 const {getcontractinfo}=require('../nodetool/id-readcontracts');
 const ethers = require('ethers');
-const secret = require('../../../../privateinfo/.secret.json');
+const secret = global.secret;
 
 exports.contractinfo = router.get("/", async (req, res) => {
     const contractinfo = await getcontractinfo();
@@ -31,18 +31,6 @@ exports.contractapi = router.get("/read", async (req, res) => {
         return;
     }
     params.params= JSON.parse(params.params);
-    let result = await readcontracts(params)
-    
-    
-    res.send({
-        ...result
-    });
-    return;
-});
-
-exports.readcontracts = readcontracts;
-
-async function readcontracts(params){
     let id = params.id;
     let contractname = params.contractname;
 
@@ -59,7 +47,82 @@ async function readcontracts(params){
     );
     let contractWithSigner = contract.connect(wallet);
     let tx;
+    // console.log(contract);
+    try {
+        if(params.params.length>0){
+            // tx = await contractWithSigner[params.fun](...params.params);
+            // console.log(...params.params);
+            tx = await contractWithSigner[params.fun](...params.params);
+        }else{
+            tx = await contractWithSigner[params.fun]();
+        }
+        // if(params.params.length>0){
+        //     // tx = await contractWithSigner[params.fun](...params.params);
+        //     tx = await contractWithSigner.estimateGas[params.fun](...params.params);
+        // }else{
+        //     tx = await contractWithSigner.estimateGas[params.fun]();
+        // }
+    } catch (error) {
+        console.log(error);
+        res.send({
+            success:false,
+            data:{
+                account:account.address,
+                result:tx
+            },
+        });
+    }
+    
+    
+    res.send({
+        success:true,
+        data:{
+            account:account.address,
+            result:tx
+        },
+    });
 
+    // params.params= JSON.parse(params.params);
+    // console.log(...(params.params));
+    // res.send({
+    //     success:true,
+    //     data:{
+    //         result:params.params,
+    //         tx:params.params,
+    //     },
+    // });
+    return;
+});
+
+exports.checkorderid = router.post("/postread", async (req, res) => {
+    var params = req.body;
+    // console.log(params);
+    
+    let check =["id","contractname","fun","params"];
+    if(!check.every(key=>key in params)){
+        res.send({
+            success:false,
+            error:"error params"
+        });
+        return;
+    }
+    // params.params= JSON.parse(params.params);
+    let id = params.id;
+    let contractname = params.contractname;
+
+    const contractinfo = await getcontractinfo();
+    var path = "m/44'/60'/0'/9/9";
+    const account = ethers.Wallet.fromMnemonic(secret.solidity.mnemonic, path);
+
+    let provider = new ethers.providers.JsonRpcProvider(contractinfo[id][contractname].network.url);
+    let wallet = new ethers.Wallet(account._signingKey(), provider);
+    let contract = new ethers.Contract(
+        contractinfo[id][contractname].address, 
+        contractinfo[id][contractname].abi, 
+        provider
+    );
+    let contractWithSigner = contract.connect(wallet);
+    let tx;
     try {
         if(params.params.length>0){
             tx = await contractWithSigner[params.fun](...params.params);
@@ -68,30 +131,22 @@ async function readcontracts(params){
         }
     } catch (error) {
         console.log(error);
-        return {
+        res.send({
             success:false,
-            account:account.address,
             data:{
+                account:account.address,
                 result:tx
             },
-        }
-        // res.send({
-        //     success:false,
-        //     account:account.address,
-        //     data:{
-        //         result:tx
-        //     },
-        // });
+        });
     }
     
-    return {
+    
+    res.send({
         success:true,
-        account:account.address,
         data:{
+            account:account.address,
             result:tx
         },
-    }
-}
-// module.exports = {
-//     readcontracts
-// }
+    });
+    return;
+});

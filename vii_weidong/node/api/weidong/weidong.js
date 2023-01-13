4bbe2c99bf2ec641440b0c4801074659c8ade5a4
ZWJKF <503755414@qq.com>
2023-01-13 10:24:32 +0800

    update

--- vii_weidong/node/api/weidong/weidong.js
+++ vii_weidong/node/api/weidong/weidong.js
@@ -14,182 +14,12 @@ contractset();
 
 let poapcontractinfo = {
     address: "0xD5e264f146661797FF7F849A56eF4CD13a5432b9",
-    erc: "erc1155",
+    erc: "erc721",
     chainid: "1030",
     chainname: "Conflux eSpace",
     blockrpc: "https://evm.confluxrpc.com",
     blockexplorer: "https://evm.confluxscan.net"
 }
-// exports.postmint = router.post("/postmint", async (req, res) => {
-//     var params = req.body;
-
-//     let check = ["account", "tokenid"];
-//     let tokenid = params.tokenid;
-//     if (!check.every(key => key in params)) {
-//         res.send({
-//             success: false,
-//             errorCode: "10914001",
-//             data: {
-//                 poapcontractinfo: {
-//                     ...baseinfo(poapcontractinfo,params),
-//                     tokenid: tokenid
-//                 }
-//             },
-//             errorMessage: "error params"
-//         });
-//         return;
-//     }
-//     params.account = toChecksumAddress(params.account);
-//     let check2 = ["key"];
-//     if (!check2.every(key => key in params)) {
-//         res.send({
-//             success: false,
-//             errorCode: "10914002",
-//             data: {
-//                 poapcontractinfo: {
-//                     ...baseinfo(poapcontractinfo,params),
-//                     tokenid: tokenid
-//                 }
-//             },
-//             errorMessage: "no key"
-//         });
-//         return;
-//     }
-//     let key_set = "8d7b3b976dd7dc3f54ab3e6d234c30ff";
-//     if (params["key"] != key_set) {
-//         res.send({
-//             success: false,
-//             errorCode: "10914003",
-//             data: {
-//                 poapcontractinfo: {
-//                     ...baseinfo(poapcontractinfo,params),
-//                     tokenid: tokenid
-//                 }
-//             },
-//             errorMessage: "error key"
-//         });
-//         return;
-//     }
-
-
-//     let sqlparams = [];
-//     for (let i in check) {
-//         sqlparams.push(params[check[i]]);
-//     }
-//     let checkorderid = "select * from mint_list where (account,tokenid) in ((?),(?))"
-//     let orderidsql = await conn.select(checkorderid, [sqlparams, [0, 0]])
-//     // console.log([params["account"],params["tokenid"]],"\n",orderidsql);
-//     // return
-//     if (orderidsql.length != 0) {
-//         res.send({
-//             success: false,
-//             errorCode: "10914004",
-//             data: {
-//                 poapcontractinfo: {
-//                     ...baseinfo(poapcontractinfo,params),
-//                     tokenid: tokenid
-//                 }
-//             },
-//             errorMessage: "Repeated order submission"
-//         });
-//         return;
-//     }
-
-//     let sqlStr = "INSERT INTO mint_list(account,tokenid)VALUES(?,?)";
-//     try {
-//         await conn.select(sqlStr, sqlparams);
-//     } catch (error) {
-//         res.send({
-//             success: false,
-//             errorCode: "10914005",
-//             data: {
-//                 poapcontractinfo: {
-//                     ...baseinfo(poapcontractinfo,params),
-//                     tokenid: tokenid
-//                 }
-//             },
-//             errorMessage: error
-//         });
-//         // sendEmailandto("Wallet:orderid error ","error orderid");
-//         return;
-//     }
-
-//     res.send({
-//         success: true,
-//         data: {
-//             success: true,
-//             poapcontractinfo: {
-//                     ...baseinfo(poapcontractinfo,params),
-//                 tokenid: tokenid
-//             }
-
-//         }
-//     });
-//     return;
-// });
-
-// exports.checkaccount = router.post("/checkaccount", async (req, res) => {
-//     var params = req.body;
-
-//     let check = ["account", "tokenid"];
-//     let tokenid = params.tokenid;
-//     if (!check.every(key => key in params)) {
-//         res.send({
-//             success: false,
-//             errorCode: "10914001",
-//             data: {
-//                 poapcontractinfo: {
-//                     ...baseinfo(poapcontractinfo,params),
-//                     tokenid: tokenid
-//                 }
-//             },
-//             errorMessage: "error params"
-//         });
-//         return;
-//     }
-//     try {
-//         let data = await getbalanceOf(params.account, params.tokenid);
-//         // console.log(data);
-//         if (data == 0) {
-//             res.send({
-//                 success: true,
-//                 data: {
-//                     success: false,
-//                     poapcontractinfo: {
-//                         ...baseinfo(poapcontractinfo,params),
-//                         tokenid: tokenid
-//                     }
-//                 }
-//             });
-//         } else {
-//             res.send({
-//                 success: true,
-//                 data: {
-//                     success: true,
-//                     poapcontractinfo: {
-//                         ...baseinfo(poapcontractinfo,params),
-//                         tokenid: tokenid
-//                     }
-//                 }
-//             });
-//         }
-//     } catch (error) {
-//         res.send({
-//             success: false,
-//             errorCode: "10914006",
-//             data: {
-//                 poapcontractinfo: {
-//                     ...baseinfo(poapcontractinfo,params),
-//                     tokenid: tokenid
-//                 }
-//             },
-//             errorMessage: "blockchain call error"
-//         });
-//     }
-// });
-
-
-
 async function ownerOf(tokenid) {
     // let contractinfo = await getcontractinfo();
     var params = new Object();
@@ -237,10 +67,10 @@ exports.phonepostmint = router.post("/phonepostmint", async (req, res) => {
             success: false,
             errorCode: "10914001",
             data: {
-                poapcontractinfo: {
+                nftinfo: [{
                     ...baseinfo(poapcontractinfo,params),
                     tokenid: tokenid
-                }
+                }]
             },
             errorMessage: "error params",
         });
@@ -252,10 +82,10 @@ exports.phonepostmint = router.post("/phonepostmint", async (req, res) => {
             success: false,
             errorCode: "10914002",
             data: {
-                poapcontractinfo: {
+                nftinfo: [{
                     ...baseinfo(poapcontractinfo,params),
                     tokenid: tokenid
-                }
+                }]
             },
             errorMessage: "no key"
 
@@ -268,10 +98,10 @@ exports.phonepostmint = router.post("/phonepostmint", async (req, res) => {
             success: false,
             errorCode: "10914003",
             data: {
-                poapcontractinfo: {
+                nftinfo: [{
                     ...baseinfo(poapcontractinfo,params),
                     tokenid: tokenid
-                }
+                }]
             },
             errorMessage: "error key"
         });
@@ -292,10 +122,10 @@ exports.phonepostmint = router.post("/phonepostmint", async (req, res) => {
             success: false,
             errorCode: "10914004",
             data: {
-                poapcontractinfo: {
+                nftinfo: [{
                     ...baseinfo(poapcontractinfo,params),
                     tokenid: tokenid
-                }
+                }]
             },
             errorMessage: "Repeated order submission"
         });
@@ -310,10 +140,10 @@ exports.phonepostmint = router.post("/phonepostmint", async (req, res) => {
             success: false,
             errorCode: "10914005",
             data: {
-                poapcontractinfo: {
+                nftinfo: [{
                     ...baseinfo(poapcontractinfo,params),
                     tokenid: tokenid
-                }
+                }]
             },
             errorMessage: error
         });
@@ -325,10 +155,10 @@ exports.phonepostmint = router.post("/phonepostmint", async (req, res) => {
         success: true,
         data: {
             success: true,
-            poapcontractinfo: {
-                    ...baseinfo(poapcontractinfo,params),
+            nftinfo: [{
+                ...baseinfo(poapcontractinfo,params),
                 tokenid: tokenid
-            }
+            }]
 
 
         }
@@ -352,10 +182,10 @@ exports.phonecheckaccount = router.post("/phonecheckaccount", async (req, res) =
             success: false,
             errorCode: "10914001",
             data: {
-                poapcontractinfo: {
+                nftinfo: [{
                     ...baseinfo(poapcontractinfo,params),
                     tokenid: tokenid
-                }
+                }]
             },
             errorMessage: "error params"
         });
@@ -367,17 +197,17 @@ exports.phonecheckaccount = router.post("/phonecheckaccount", async (req, res) =
         if (data !=params.account) {
             let sqlstr="select phone from wallet where address=?";
             let phonesql = await conn.select(sqlstr, data);
-            // console.log(phonesql,data);
+            console.log(phonesql,data);
             res.send({
                 success: true,
                 data: {
                     success: false,
-                    owner:data,
-                    phone:phonesql[0].phone,
-                    poapcontractinfo: {
+                    nftinfo: [{
                         ...baseinfo(poapcontractinfo,params),
+                        owner:data,
+                        ownertophone:phonesql[0].phone,
                         tokenid: tokenid
-                    }
+                    }]
                 }
             });
         } else {
@@ -385,11 +215,12 @@ exports.phonecheckaccount = router.post("/phonecheckaccount", async (req, res) =
                 success: true,
                 data: {
                     success: true,
-                    owner:data,
-                    poapcontractinfo: {
+                    nftinfo: [{
                         ...baseinfo(poapcontractinfo,params),
+                        owner:data,
+                        ownertophone:phonesql[0].phone,
                         tokenid: tokenid
-                    }
+                    }]
                 }
             });
         }
@@ -398,10 +229,10 @@ exports.phonecheckaccount = router.post("/phonecheckaccount", async (req, res) =
             success: false,
             errorCode: "10914006",
             data: {
-                poapcontractinfo: {
+                nftinfo: [{
                     ...baseinfo(poapcontractinfo,params),
                     tokenid: tokenid
-                }
+                }]
             },
             errorMessage: "blockchain call error"
         });
@@ -432,17 +263,25 @@ exports.phonegetnft = router.post("/phonegetnft", async (req, res) => {
         return;
     }
     try {
-        let nftinfo = await getaccountnft(account.address);
+        let nfts = await getaccountnft(account.address);
+        // console.log(nfts);
+        let nftinfo = new Array()
+        for (let i = 0; i < nfts.tokenids.length; i++) {
+            nftinfo.push(
+                {
+                    owner:account.address,
+                    tokenid:nfts.tokenids[i],
+                    ...baseinfo(poapcontractinfo,params),
+                }
+            )
+        }
         res.send({
             success: true,
             data: {
                 success: true,
-                nftinfo:nftinfo,
-                poapcontractinfo: {
-                    ...baseinfo(poapcontractinfo,params),
-                }
-    
-    
+                nftinfo:[
+                    ...nftinfo
+                ],
             }
         });
         return;
@@ -450,11 +289,6 @@ exports.phonegetnft = router.post("/phonegetnft", async (req, res) => {
         res.send({
             success: false,
             errorCode: "10914006",
-            data: {
-                poapcontractinfo: {
-                    ...baseinfo(poapcontractinfo,params),
-                }
-            },
             errorMessage: "blockchain call error"
         });
     }
